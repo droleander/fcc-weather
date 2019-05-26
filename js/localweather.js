380e9193287ac6642c611f7b055d33cdccfe45c8
Leandro <lileandermail+gh+@gmail.com>
2019-05-26 23:39:54 +0800

    Updated

--- js/localweather.js
+++ js/localweather.js
@@ -3,7 +3,7 @@
 // https://cors-anywhere.herokuapp.com/
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 	getGeoInfo();
 });
 
@@ -43,9 +43,11 @@ async function getWeatherInfo(lat, lon) {
 		let temp = data.main.temp;
 		let weather = data.weather[0].description;
 		weather = weather.substr(0, 1).toUpperCase() + weather.substr(1);
+		let sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+		let sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
 		let wind = data.wind.speed;
 
-		displayWeather(loc, icon, weather, temp, wind);
+		displayWeather(loc, icon, weather, temp, sunrise, sunset, wind);
 
 	} catch (ex) {
 		alert(`An error has occurred.\nPlease try again later.`);
@@ -53,11 +55,13 @@ async function getWeatherInfo(lat, lon) {
 }
 
 
-function displayWeather(loc, icon, weather, temp, wind) {
+function displayWeather(loc, icon, weather, temp, sunrise, sunset, wind) {
 	$("#dspLoc").html(loc);
 	$(".dspIcon").attr("src", icon);
 	$("#dspWeather").html(weather);
 	$("#dspTempValue").html(temp);
+	$("#dspSunriseTime").html(sunrise);
+	$("#dspSunsetTime").html(sunset);
 	$("#dspWindValue").html(wind);
 }
 
@@ -85,3 +89,51 @@ function convertTemp() {
 
 	$("#dspTempValue").html(currTemp);
 }
+
+/* Sample JSON Response
+
+{
+	"coord": {
+		"lon": 121,
+		"lat": 14.51
+	},
+	"weather": [
+	{
+		"id": 500,
+		"main": "Rain",
+		"description": "light rain",
+		"icon": "10n"
+	}
+	],
+	"base": "stations",
+	"main": {
+		"temp": 26.98,
+		"pressure": 1011,
+		"humidity": 88,
+		"temp_min": 25.56,
+		"temp_max": 29.44
+	},
+	"visibility": 8000,
+	"wind": {
+		"speed": 1,
+		"deg": 60
+	},
+	"clouds": {
+		"all": 90
+	},
+	"dt": 1558880302,
+	"sys": {
+		"type": 1,
+		"id": 8160,
+		"message": 0.0062,
+		"country": "PH",
+		"sunrise": 1558819596,
+		"sunset": 1558865979
+	},
+	"timezone": 28800,
+	"id": 7091218,
+	"name": "Moonwalk II",
+	"cod": 200
+	}
+
+*/
