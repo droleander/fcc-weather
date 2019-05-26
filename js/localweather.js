9ca6a7d4b6731faad1156045133c2cef887b30a7
Leandro <lileandermail+gh+@gmail.com>
2019-05-26 20:37:12 +0800

    Updated

--- js/localweather.js
+++ js/localweather.js
@@ -3,15 +3,17 @@
 // https://cors-anywhere.herokuapp.com/
 
 
-$(document).ready(() => getGeoInfo());
+$(document).ready(function() {
+	getGeoInfo();
+});
 
 
 function getGeoInfo() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(geoData) {
+		navigator.geolocation.getCurrentPosition(function (geoData) {
 			let lat = geoData.coords.latitude;
 			let lon = geoData.coords.longitude;
-			
+
 			getWeatherInfo(lat, lon);
 		});
 	} else {
@@ -21,9 +23,9 @@ function getGeoInfo() {
 
 
 async function getWeatherInfo(lat, lon) {
-    const OWMKEY = "9e63e3db08ca3fc65ea3925879bdc7b7";
-    const OWMAPI = "https://api.openweathermap.org/data/2.5/weather";
-	
+	const OWMKEY = "9e63e3db08ca3fc65ea3925879bdc7b7";
+	const OWMAPI = "https://api.openweathermap.org/data/2.5/weather";
+
 	try {
 		let data = await $.ajax({
 			url: OWMAPI,
@@ -35,16 +37,16 @@ async function getWeatherInfo(lat, lon) {
 			},
 			dataType: 'json'
 		});
-		
+
 		let loc = data.name;
 		let icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 		let temp = data.main.temp;
 		let weather = data.weather[0].description;
 		weather = weather.substr(0, 1).toUpperCase() + weather.substr(1);
 		let wind = data.wind.speed;
-		
+
 		displayWeather(loc, icon, weather, temp, wind);
-		
+
 	} catch (ex) {
 		alert(`An error has occurred.\nPlease try again later.`);
 	}
@@ -52,36 +54,34 @@ async function getWeatherInfo(lat, lon) {
 
 
 function displayWeather(loc, icon, weather, temp, wind) {
-    $("#dspLoc").html(loc);
-    $(".dspIcon").attr("src", icon);
-    $("#dspWeather").html(weather);
-    $("#dspTempValue").html(temp);
-    $("#dspWindValue").html(wind);
+	$("#dspLoc").html(loc);
+	$(".dspIcon").attr("src", icon);
+	$("#dspWeather").html(weather);
+	$("#dspTempValue").html(temp);
+	$("#dspWindValue").html(wind);
 }
 
 
 function convertTemp() {
-    let bttnValue = $("#dspButton").attr("value");
-    let currTemp = parseFloat($("#dspTempValue").html());
-	
-    // C = (5 / 9) * (F - 32); F = ((9 / 5) * C) + 32; //
-	
-    if (bttnValue === "degC") {
-        currTemp = parseFloat((((9 / 5) * currTemp) + 32).toFixed(2));
-		
-        // display info //
-        $("#dspTempUnit").html("F");
-        $("#dspButton").attr("value", "degF").html("&deg;C");
-		
-    } else {
-        currTemp = parseFloat(((5 / 9) * (currTemp - 32)).toFixed(2));
-		
-        // display info //
-        $("#dspTempUnit").html("C");
-        $("#dspButton").attr("value", "degC").html("&deg;F");
-    }
-	
-    $("#dspTempValue").html(currTemp);
+	let bttnValue = $("#dspButton").attr("value");
+	let currTemp = parseFloat($("#dspTempValue").html());
+
+	// C = (5 / 9) * (F - 32); F = ((9 / 5) * C) + 32; //
+
+	if (bttnValue === "degC") {
+		currTemp = parseFloat((((9 / 5) * currTemp) + 32).toFixed(2));
+
+		// display info //
+		$("#dspTempUnit").html("F");
+		$("#dspButton").attr("value", "degF").html("&deg;C");
+
+	} else {
+		currTemp = parseFloat(((5 / 9) * (currTemp - 32)).toFixed(2));
+
+		// display info //
+		$("#dspTempUnit").html("C");
+		$("#dspButton").attr("value", "degC").html("&deg;F");
+	}
+
+	$("#dspTempValue").html(currTemp);
 }
-
-
